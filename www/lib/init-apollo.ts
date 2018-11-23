e0a82e2c050ffb77570d31313f14b2cf2778dc84
jchen <jchen@warriors.lan>
2018-11-22 23:28:43 -0800

    apollo query

--- www/lib/init-apollo.ts
+++ www/lib/init-apollo.ts
@@ -18,14 +18,13 @@ if (!isBrowser) {
 
 function create(initialState?: any) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
+  const localhost = 'http://localhost:4000';
+  const prodhost = 'https://tenstep.net';
   return new ApolloClient({
     connectToDevTools: isBrowser,
     ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri:
-        process.env.NODE_ENV === 'production'
-          ? 'https://server.codeponder.com/graphql'
-          : 'http://localhost:4000/graphql',
+      uri: process.env.NODE_ENV === 'production' ? prodhost : localhost,
       credentials: 'include'
     }),
 
